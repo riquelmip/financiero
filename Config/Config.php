1f71dbb20d9afcdfa0efd18bef843b18af2539eb
Rivs Elí David <vr17044@ues.edu.sv>
2022-01-17 09:44:43 -0600

    Agregar productos

--- Config/Config.php
+++ Config/Config.php
@@ -8,7 +8,7 @@
 
 	//Datos de conexión a Base de Datos
 	const DB_HOST = "localhost";
-	const DB_NAME = "db_ferreteria";
+	const DB_NAME = "db_financiero";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
 	const DB_CHARSET = "utf8";
